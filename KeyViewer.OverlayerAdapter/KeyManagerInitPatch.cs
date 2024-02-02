9fcab8aaa8680ac81be2cfa3d2fc8197408a66cc
C#Newbie <dongsu45681111@gmail.com>
2024-02-03 00:41:32 +0900

    overlayer adapter

---
+++ KeyViewer.OverlayerAdapter/KeyManagerInitPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using KeyViewer.Core.TextReplacing;
+using KeyViewer.Unity;
+using Overlayer.Core;
+using System.Linq;
+
+namespace KeyViewer.OverlayerAdapter
+{
+    [HarmonyPatch(typeof(KeyManager), "Init")]
+    public static class KeyManagerInitPatch
+    {
+        public static void Postfix(KeyManager __instance)
+        {
+            __instance.AllTags.AddRange(
+                TagManager.All.Select(t =>
+                {
+                    var tag = new Tag(t.Name);
+                    var go = t.GetterOriginal;
+                    if (go.Name == "Invoke")
+                        return tag.SetGetter(t.GetterOriginalDelegate);
+                    else return tag.SetGetter(go);
+                }));
+        }
+    }
+}
---
+++ KeyViewer.OverlayerAdapter/Main.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using static UnityModManagerNet.UnityModManager;
+
+namespace KeyViewer.OverlayerAdapter
+{
+    public static class Main
+    {
+        public static void Load(ModEntry modEntry)
+        {
+            new Harmony(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
