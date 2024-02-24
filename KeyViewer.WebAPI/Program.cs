6eb76b1e2078efe9e717a1239884bdd50a013daf
C#Newbie <dongsu45681111@gmail.com>
2024-02-25 00:38:58 +0900

    asdf

--- KeyViewer.WebAPI/Program.cs
+++ KeyViewer.WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using KeyViewer.WebAPI.Core.Utils;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+using System.Net;
 using System.Threading.RateLimiting;
 
 namespace KeyViewer.WebAPI
@@ -16,17 +20,49 @@ namespace KeyViewer.WebAPI
             {
                 options.InputFormatters.Insert(0, new BinaryInputFormatter());
             });
-            builder.Services.AddRateLimiter(_ => _
-            .AddSlidingWindowLimiter(policyName: "sliding", options =>
+
+            builder.Services.AddRateLimiter(limiterOptions =>
             {
-                // 요청 허용 갯수 : 10
-                options.PermitLimit = 10;
-                // 10초 동안 최대 PermitLimit개의 요청만 처리 가능
-                options.Window = TimeSpan.FromSeconds(10);
-                options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-                // 제한시 5개의 요청만 대기열에 추가
-                options.QueueLimit = 100;
-            }));
+                limiterOptions.OnRejected = (ctx, cTok) =>
+                {
+                    if (ctx.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    {
+                        ctx.HttpContext.Response.Headers.RetryAfter =
+                            ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+                    }
+                    ctx.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                    return ValueTask.CompletedTask;
+                };
+                // 글로벌 속도 제한 설정
+                limiterOptions.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, IPAddress>(context =>
+                {
+                    // 요청 IPAddress
+                    IPAddress? remoteIpAddress = IPAddress.Parse(context.GetIpAddress() ?? "127.0.0.1");
+
+                    // 요청된 IPAddress가 루프백이 아닌 경우
+                    if (IPAddress.IsLoopback(remoteIpAddress!) == false)
+                    {
+                        // IPAddress에 대해 속도 제한 설정
+                        return RateLimitPartition.GetSlidingWindowLimiter
+                        (remoteIpAddress!, _ =>
+                        new SlidingWindowRateLimiterOptions
+                        {
+                            // 요청 허용 갯수 : 100
+                            PermitLimit = 3,
+                            // 창 이동시간 30초
+                            Window = TimeSpan.FromSeconds(5),
+                            // 창 분할 세그먼트 갯수
+                            SegmentsPerWindow = 3,  // 1개의 세그먼트 : 5s / 3
+                            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                            // 제한시 3개의 요청만 대기열에 추가
+                            QueueLimit = 3,
+                        });
+                    }
+
+                    // 루프백 IPAddress는 속도 제한 없음.
+                    return RateLimitPartition.GetNoLimiter(IPAddress.Loopback);
+                });
+            });
 
             var app = builder.Build();
 
